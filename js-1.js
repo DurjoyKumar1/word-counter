ac7bb4619364bf8df5686620cb089cd18679e089
DurjoyKumar <durjoy.uk6@gmail.com>
2024-06-12 12:52:17 +0600

    v4

--- js-1.js
+++ js-1.js
@@ -1,7 +1,10 @@
 //document.addEventListener('DOMContentLoaded', function() {
 let textInput= document.querySelector("#text-place");
 let cnt =document.querySelector(".counter");
+let resetBtn =document.querySelector(".reset");
 
+
+let text =cnt.innerText;
 let counter = 1;
 
 function wordCounter(){
@@ -13,9 +16,22 @@ textInput.addEventListener('input', ()=>{
         }
     }
     textInput.disabled=true;
-    console.log(cnt.innerText=cnt.innerText + counter);
+    cnt.innerText=cnt.innerText + counter;
 
 });
 }
+
+function reset(){
+    resetBtn.addEventListener("click",()=>{
+        counter = 1;
+        textInput.disabled=false;
+        textInput.value="";
+        cnt.innerText=text;
+        
+    });
+
+}
+
+reset();
 wordCounter();
 //});
